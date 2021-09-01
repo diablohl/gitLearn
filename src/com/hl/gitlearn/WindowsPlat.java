dd2cc61e57420dc2c9116570b87d0289726170e4
diablohl <hldiablo@hotmail.com>
2021-09-01 18:36:24 +0800

    windows  firstchange

--- src/com/hl/gitlearn/WindowsPlat.java
+++ src/com/hl/gitlearn/WindowsPlat.java
@@ -3,5 +3,7 @@ package com.hl.gitlearn;
 public class WindowsPlat {
     public static void main(String[] args) {
         System.out.println("windowsc create");
+        System.out.println("windowsc first change");
+
     }
 }
