5bdf52e9a76c345df589c889b40b3d818269b566
hl <hldiablo@hotmail.com>
2021-09-01 20:54:20 +0800

    mac first change

--- src/com/hl/gitlearn/WindowsPlat.java
+++ src/com/hl/gitlearn/WindowsPlat.java
@@ -4,6 +4,7 @@ public class WindowsPlat {
     public static void main(String[] args) {
         System.out.println("windowsc create");
         System.out.println("windowsc first change");
+        System.out.println("windowsc second change");
 
     }
 }
